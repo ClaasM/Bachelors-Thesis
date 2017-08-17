6c8902c4f035842eb842d7bead55632bf9ccac22
ga96sag <ga96sag@tum.de>
2017-08-17 11:17:48 +0200

    Added a netcat consumer to be used during debugging

---
+++ dashboard/streaming/netcat_consumer.py
@@ -0,0 +1,45 @@
+import os
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+from streaming import spark_functions
+
+os.environ['PYSPARK_SUBMIT_ARGS'] \
+    = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 pyspark-shell'
+
+
+class NetcatConsumer(object):
+    """
+    A class used for debugging that works with netcat instead of kafka
+    """
+
+    def __init__(self):
+        sc = SparkContext.getOrCreate()
+        sc.setLogLevel("ERROR")
+        self.ssc = StreamingContext(sc, 1)  # 1 second window
+        self.ssc.checkpoint("./checkpoints")
+
+    def start(self, sid):
+        # ONLY USE GLOBAL FUNCTIONS!
+        # Create the stream
+        stream = self.ssc.socketTextStream("localhost", 9999)
+        # TODO test functions here
+        stream.pprint()
+
+        # Start the streaming
+        self.ssc.start()
+
+    def stop(self):
+        self.ssc.stop()
+        print("Streaming context stopped")
+
+    def await_termination(self):
+        """
+        Blocking.
+        This can be used during testing/in notebooks to prevent the script from terminating.
+        Not needed for use on the server.
+        :return:
+        """
+        self.ssc.awaitTermination()
